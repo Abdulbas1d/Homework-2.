0f1d11a10b600d0b2ea177ea7cb6b91e07be06b8
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-03-29 03:08:00 +0500

    solution first example

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// HOMEWORK
+// 1-HAFTA KUNLARI
+// let a = 5;
+// let result;
+// switch (a) {
+//     case 1:
+//         result="Dushanba"
+//         break; 
+//     case 2:
+//         result="Seshanba"
+//         break; 
+//     case 3:
+//         result="Chorshanba"
+//         break; 
+//     case 4:
+//         result="Payshanba"
+//         break; 
+//     case 5:
+//         result="Juma"
+//         break;
+//     case 6:
+//         result="Shanba"
+//         break;
+//     case 7:
+//         result="Yakshanba"
+//         break;
+//     default:
+//         result="Bunday hafta kuni yo'q"
+//         break;
+// }
+
+// console.log(result);
